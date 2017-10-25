3a4e7db786fb3f3a58c152356aaa1332d15b855b
Ben Hoffman <bah8892@g.rit.edu>
2017-10-25 11:40:16 -0400

    Added the project to the repo

---
+++ MiniMapDemo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 10f;
+    public float rotateSpeed = 10f;
+
+    private Vector3 movement;
+    private float rotation;
+
+    void Update()
+    {
+        movement.z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        rotation = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+    }
+
+    /// <summary>
+    /// Apply the motion in the fixed update method
+    /// </summary>
+    void FixedUpdate()
+    {
+        transform.Translate(movement, Space.Self);
+        transform.Rotate(0f, rotation, 0f);
+    }
+}
